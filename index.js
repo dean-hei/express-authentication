1144872db6ecc889c1f7377b737feb64193b80c6
deanjheikkila <deanjheikkila@gmail.com>
2020-02-21 12:27:00 -0800

    added login authentication functionality

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
+const session = require('express-session');
+const passport = require('./config/ppConfig');
+const flash = require('connect-flash');
+const isLoggedIn = require('./middleware/isLoggedIn')
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -10,15 +15,33 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 app.use(ejsLayouts);
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use(function(req, res, next) {
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.get('/', function(req, res) {
+  console.log(`User is ${req.user ? req.user.name : "not logged in"}`);
   res.render('index');
 });
 
-app.get('/profile', function(req, res) {
-  res.render('profile');
+app.get('/profile', isLoggedIn, function(req, res) {
+  res.render('profile', { user: req.user });
 });
 
 app.use('/auth', require('./controllers/auth'));
+app.use('/', isLoggedIn, require('./controllers/test'));
 
 var server = app.listen(process.env.PORT || 3000);
 
